2f7b1dfddb096c021b5a482b7c69c75c19884656
Mark Biesheuvel <mail@markbiesheuvel.nl>
2017-02-02 19:55:59 +0100

    Set default route and exception handler

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,12 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public function index()
+    {
+        $data = [
+            'foo' => 'bar',
+        ];
+
+        return response()->json($data);
+    }
 }
